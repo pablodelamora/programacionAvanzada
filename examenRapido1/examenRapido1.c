2cc14d24641d9bbf7d0374fc3dc1e3d0b0fb32ac
root <root@localhost.localdomain>
2016-01-21 12:29:10 -0600

    Examen Rapido 1

--- examenRapido1/examenRapido1.c
+++ examenRapido1/examenRapido1.c
@@ -5,7 +5,9 @@
 #include <stdlib.h>
 #define N 4
 
-
+void ordena(char*, int *, int (*)(int, int));
+int asc(int, int);
+int desc(int, int);
 
 
 int numPers=0;
@@ -13,6 +15,7 @@ int numPers=0;
 
 int main(int argc, const char * argv[])
 {
+    char *letras;
     char * cadena;
     int *count;
     int longitud;
@@ -21,7 +24,15 @@ int main(int argc, const char * argv[])
     scanf("%d", &longitud);
     
     cadena = (char *) malloc(sizeof(char) * longitud);
+    letras = (char*) malloc (sizeof(char)*26);
     count = (int*) malloc (sizeof(int)*26);
+    
+    char* ayuda = letras;
+    for (;ayuda<(letras+26);ayuda++){
+     
+        *ayuda=(ayuda-letras)+'a';
+        printf("%c",*ayuda);
+    }
     
     printf("Entre una cadena: ");
     //scanf("%s", cadena);
@@ -39,27 +50,72 @@ int main(int argc, const char * argv[])
     copia++;
     }
     
+    
+    ordena(letras,count,asc);
+    
+    
     int* c= count;
     for (;c<(count+26);c++){
         if (*c != 0){
-        printf("%c occurs %d times ", ((c-count)+'a'),  (*c));
+            printf("%c occurs %d times ", (*letras),  (*c));
+        //printf("%c occurs %d times ", ((c-count)+'a'),  (*c));
         int i=0;
         for (i=0; i<*c;i++){
             printf("*");
         }
         printf("\n");
-        cadena++;
+//        cadena++;
         }
+        letras++;
     }
     
     
-    
-    
-    
-  //  free (count);
-  //  free(cadena);
+    free(letras);
+    free (count);
+    free(cadena);
     return 0;
 }//Cierre de main
 
+
+
+
+
+
+//////
+void ordena(char * letras,int * numeros, int criterio(int, int))
+{
+    int i, j, temp;
+    char temp1;
+    
+    for(i = 0; i < N; ++i)
+    {
+        for(j = N-1; j > i; --j)
+        {
+            if (criterio(numeros[j],numeros[j-1])) {
+                
+                printf("%d",numeros[j-1]);
+                temp = numeros[j-1];
+                temp1=letras[j-1];
+                
+                numeros[j-1] = numeros[j];
+                letras[j-1] = letras[j];
+                
+                numeros[j] = temp;
+                letras[j]=temp1;
+                printf("-- %d\n",numeros[j-1]);
+            }
+        }
+    }
+}
+
+int asc(int a, int b)
+{
+    return a < b;
+}
+
+int desc(int a, int b)
+{
+    return b < a;
+}
 
 
