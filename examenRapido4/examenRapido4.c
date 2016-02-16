4a7c6229b6f70178fa3ecece65c7fa6d62cb4102
root <root@localhost.localdomain>
2016-02-15 18:00:22 -0600

    Examen

--- examenRapido4/examenRapido4.c
+++ examenRapido4/examenRapido4.c
@@ -22,42 +22,50 @@ int main(int argc, char * argv[])
         pid_t pid;
         for(i = 0; i < num-1; ++i) {
             pid = fork();
+//            printf("%d\n", i);
             if(pid == -1)
             {
                 printf("Error al crear hijo\n");
                 break;
             }
             else if(pid == 0) {
+//                printf("hijo \n");
+//                printf("%d", i);
                 if(i == num-2) {
-                    aux1 = i+1;
-                    aux2 = i;
+                    aux2 = i+1;
+                    aux1 = i;
+
+//                    printf("3= %d", i);
                     close((*(arrPipe+aux1))[0]);
                     write((*(arrPipe+aux1))[1], &token, sizeof(char));
                     break;
                 }
             }
             else {
-                aux1 = i;
+//                printf("padre \n");
+//                printf("%d", i);
+                aux2 = i;
                 if(i == 0){
-                    aux2 = num-1;
+                    aux1 = num-1;
+//                    printf("1= %d", i);
                 }
                 else{
-                    aux2 = i-1;
+                    aux1 = i-1;
+//                    printf("2= %d", i);
                 }
                 break;
             }
         }
 
         char tokenLeido;
+//        printf("prueba\n");
 
 
-        int loop=1;
-        while(loop) {
+        while(1) {
             close((*(arrPipe+aux2))[1]);
             read((*(arrPipe+aux2))[0], &tokenLeido, sizeof(char));
             printf("Soy el proceso con PID %d y recibi el testigo %c el cual tendre por 5 segundos\n", getpid(), tokenLeido);
             sleep(5);
-
             close((*(arrPipe+aux1))[0]);
             write((*(arrPipe+aux1))[1], &tokenLeido, sizeof(char));
             printf("Soy el proceso con PID %d y acabo de enviar el testigo %c\n\n", getpid(), token);
